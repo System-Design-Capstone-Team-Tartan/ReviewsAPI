7b8a6e1d88f97834a1518a5fdedf36c31e951dff
Alex Kenney <dareitus@gmail.com>
2022-01-19 15:44:22 -0500

    route management

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 
-// const mountRoutes = require('./routes');
-
 const app = express();
-// mountRoutes(app);
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.listen(port, () => {
+  console.log(`Server Running at: http://localhost:${port}`);
 });
 
-app.listen(port, () => {
-  console.log(`We are LIVE at http://localhost:${port}`);
+app.get('/', (req, res) => {
+  // page, count, sort, product_id
+  res.send('Test Index');
+});
+
+app.get('/reviews/', (req, res) => {
+  // page, count, sort, product_id
+  res.send('Review Get Request');
+});
+
+app.get('/reviews/meta', (req, res) => {
+  // product_id
+  res.send('Review Meta Get Request');
+});
+
+app.post('/reviews', (req, res) => {
+  // product_id, rating, summary, body, recommend, name, email, photos, characteristics
+  // integer,    int,    text,    text,  bool,    text,  text,  [text], object
+  res.send('Review Post Request');
+});
+
+app.put('/reviews/:review_id/helpful', (req, res) => {
+  // review_id
+  res.send('Helpful Put Request');
+});
+
+app.get('/reviews', (req, res) => {
+  // review_id
+  res.send('Report Put Request');
 });
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-// const reviews = require('./reviews');
-
-// const meta = require('./meta');
-
-// module.exports = (app) => {
-//   app.use('/reviews', reviews);
-//   app.use('/meta', meta);
-// };
